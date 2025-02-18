e940e6e1d1fb6ca31e0bbb75deab8af72679ef38
vishnud05 <vishnu290511@gmail.com>
2025-02-18 15:05:36 +0530

    authorize ws backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,25 @@
 import { WebSocketServer } from "ws";
+import { JWT_SECRET } from "@repo/shared-server/config";
+import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({
   port: 8080,
 });
 
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    console.log(message);
-    ws.send("Hello, client! How are you?");
-  });
+wss.on("connection", (ws, request) => {
+  if (request.url) {
+    const queryParams = new URLSearchParams(request.url.split("?")[1]);
+    const token = queryParams.get("token") || "";
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+
+    if (decoded && typeof decoded != "string" && decoded.userId) {
+      ws.on("message", (message) => {
+        console.log(message);
+        ws.send("Hello, client! How are you?");
+      });
+    }
+  } else {
+    ws.close();
+    return;
+  }
 });
